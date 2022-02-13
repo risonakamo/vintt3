3f028197f90ffa9d5d189853a96d2e312fa49a5d
risonakamo <hmykanae@gmail.com>
2022-02-13 16:52:30 -0500

    more process testing

--- src/processtest.rs
+++ src/processtest.rs
@@ -1,24 +1,56 @@
 #![allow(non_snake_case)]
 #![allow(unused_variables)]
 
-use sysinfo::{System,SystemExt,ProcessExt,Process,Pid};
+use sysinfo::{System,SystemExt,ProcessExt};
+use tokio::time::{interval,Interval};
+use std::time::Duration;
 use std::collections::HashSet;
 
-fn main()
+#[tokio::main]
+async fn main()
 {
-    let mut system=System::new_all();
-    system.refresh_processes();
+    println!("waiting for process...");
 
-    let targetExe:&str="notepad.exe";
+    let foundProcess:String=waitForAProcess(vec![
+        "notepad.exe",
+        "wt.exe"
+    ]).await;
 
-    let processNames:HashSet<String>=HashSet::from_iter(
-        system.processes().iter().map(|(i,x):(&Pid,&Process)|->String {
-            return x.name().to_string();
-        })
-    );
+    println!("found {}",foundProcess);
+}
 
-    if processNames.contains(targetExe)
+/// wait for a process in the given list to exist. CONSUMES the input vector
+async fn waitForAProcess(targets:Vec<&str>)->String
+{
+    let mut system:System=System::new_all();
+    let mut timer:Interval=interval(Duration::from_secs(3));
+
+    let targetSet:HashSet<&str>=HashSet::from_iter(targets);
+
+    loop
     {
-        println!("found target");
+        timer.tick().await;
+        system.refresh_processes();
+
+        let foundProcess:String=someProcessExists(&targetSet,&system);
+
+        if foundProcess.len() > 0
+        {
+            return foundProcess;
+        }
     }
+}
+
+/// using a sysinfo system, see if a process in the process targets exists. if not, returns empty string
+fn someProcessExists(processTargets:&HashSet<&str>,system:&System)->String
+{
+    for (i,x) in system.processes()
+    {
+        if processTargets.contains(x.name())
+        {
+            return x.name().to_string();
+        }
+    }
+
+    return "".to_string();
 }
