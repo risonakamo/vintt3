edf8f273f5d274e527e087d51c7e7c8225aecb9c
risonakamo <hmykanae@gmail.com>
2022-02-19 01:14:11 -0500

    test tokio tasks tried some pathing

--- src/vintt3.rs
+++ src/vintt3.rs
@@ -1,10 +1,19 @@
+#![allow(non_snake_case)]
+
+use std::env::current_dir;
+use std::path::PathBuf;
+
 use vintt3::VinttWatcher::VinttWatcher;
 use vintt3::apis::vintt_config_api::getVinttConfig;
 
 #[tokio::main]
 async fn main()
 {
-    let mut watcher:VinttWatcher=VinttWatcher::new("time.yml");
+    let currentDir:PathBuf=current_dir().unwrap();
 
-    watcher.watch(getVinttConfig("./config/vintt_config.yml").unwrap()).await;
+    let mut watcher:VinttWatcher=VinttWatcher::new(currentDir.join("time.yml").to_str().unwrap());
+
+    watcher.watch(getVinttConfig(
+        currentDir.join("vintt_config.yml").to_str().unwrap()
+    ).unwrap()).await;
 }
--- src/warptest.rs
+++ src/warptest.rs
@@ -1,8 +1,16 @@
 use warp::Filter;
 
+use vintt3::VinttWatcher::VinttWatcher;
+use vintt3::apis::vintt_config_api::getVinttConfig;
+
 #[tokio::main]
 async fn main()
 {
+    tokio::spawn(async {
+        let mut watcher:VinttWatcher=VinttWatcher::new("time.yml");
+        watcher.watch(getVinttConfig("./config/vintt_config.yml").unwrap()).await;
+    });
+
     let test=warp::path("test").map(||->String {
         return "hello".to_string();
     });
