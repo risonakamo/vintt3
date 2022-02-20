8144baa72d7ee67e315e4056738d0340c9f230cb
risonakamo <hmykanae@gmail.com>
2022-02-20 13:56:41 -0500

    get current watch

--- lib/VinttWatcher.rs
+++ lib/VinttWatcher.rs
@@ -1,8 +1,9 @@
 use tokio::time::{interval,Interval};
 use tokio::task::JoinHandle;
 use std::time::Duration;
-use std::collections::HashSet;
+use std::collections::{HashSet,HashMap};
 use std::sync::{Arc,Mutex};
+use serde::Serialize;
 
 use crate::VinttConfig::{VinttConfig,VinttItem};
 use crate::process_watch::waitForAProcess;
@@ -14,11 +15,25 @@ pub struct VinttWatcher
 {
     timefile:Arc<Mutex<String>>,
 
+    watchProgram:Arc<Mutex<String>>,
     pub categories:Arc<Mutex<HashSet<String>>>,
 
     currentCategory:Arc<Mutex<String>>,
     elapsedTime:Arc<Mutex<u64>>,
     categoryTime:Arc<Mutex<u64>>
+}
+
+/// current watch information
+#[derive(Serialize,Default)]
+pub struct CurrentWatch
+{
+    name:String,
+
+    currentTime:u64,
+    currentCategory:String,
+
+    // all categories of the current watch and their times
+    categories:HashMap<String,u64>
 }
 
 impl VinttWatcher
@@ -29,9 +44,10 @@ impl VinttWatcher
         return Self {
             timefile:Arc::new(Mutex::new(timefile.to_string())),
 
+            watchProgram:Arc::new(Mutex::new(String::default())),
             categories:Arc::new(Mutex::new(HashSet::default())),
 
-            currentCategory:Arc::new(Mutex::new("".to_string())),
+            currentCategory:Arc::new(Mutex::new(String::default())),
             elapsedTime:Arc::new(Mutex::new(0)),
             categoryTime:Arc::new(Mutex::new(0))
         };
@@ -48,6 +64,7 @@ impl VinttWatcher
         let elapsedTimeArc=self.elapsedTime.clone();
         let categoryTimeArc=self.categoryTime.clone();
         let timefileArc=self.timefile.clone();
+        let watchProgramArc=self.watchProgram.clone();
 
         return tokio::spawn(async move {
             // get all processes to watch for
@@ -56,6 +73,7 @@ impl VinttWatcher
             // wait until found a process
             let foundProcess:String=waitForAProcess(configProcesses).await;
             println!("tracking: {}",foundProcess);
+            *(watchProgramArc.lock().unwrap())=foundProcess.clone();
 
             // set the track item to be that item
             let trackItem:VinttItem=config.track_items.get(&foundProcess).unwrap().clone();
@@ -95,5 +113,23 @@ impl VinttWatcher
         *(self.currentCategory.lock().unwrap())=newCategory.to_string();
         *(self.categoryTime.lock().unwrap())=0;
         return Ok(());
+    }
+
+    /// get the current watch. name will be empty if there is no watch
+    pub fn getCurrentWatch(&self)->CurrentWatch
+    {
+        if self.watchProgram.lock().unwrap().len() == 0
+        {
+            return CurrentWatch::default();
+        }
+
+        return CurrentWatch {
+            name:self.watchProgram.lock().unwrap().clone(),
+
+            currentTime:self.elapsedTime.lock().unwrap().clone(),
+            currentCategory:self.currentCategory.lock().unwrap().clone(),
+
+            categories:HashMap::default()
+        };
     }
 }
